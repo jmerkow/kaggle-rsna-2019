42482acd7737baab029ac80cbcd8f796fa099caf
arvind.m.vepa <arvind.m.vepa@gmail.com>
2019-10-09 18:53:40 -0700

    bug

--- kaggle_lib/pytorch/sampler.py
+++ kaggle_lib/pytorch/sampler.py
@@ -6,6 +6,8 @@ class LabelSampler(Sampler):
     def __init__(self, ids, data, label, cur_epoch=0, start_pos_freq = 1.0, end_pos_freq=1.0, cycle_length=5.0):
         idxs = np.array(ids.keys())
         labels = np.array([data[img_id]['label__' + label] for img_id in ids.values()])
+        print(labels)
+        print(idxs)
         self.pos_idxs = idxs[np.nonzero(labels)]
         self.neg_idxs = idxs[np.nonzero(labels == 0)]
 
